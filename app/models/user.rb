05ccbfe1e3008a37d05dc4a36c22171540c44b62
Shubham Kumar <shubham01384@gmail.com>
2022-01-26 15:23:12 +0530

    Code changed according to my local machine

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+    validates :name, presence: true
+    validates :mobile_number, format: { with: /\d[0-9]\)*\z/,
+         message: "only allows numbers" }
+    validates :email, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i ,message: "proper email" }
+    validates :address, presence: true
+    
+end
